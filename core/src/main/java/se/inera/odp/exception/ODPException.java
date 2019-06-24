37416f9af64dc4a2b6881029971708e0d87b6240
hanswiklund <hans.wiklund@kentor.se>
2019-06-24 13:49:40 +0200

    refaktorering

---
+++ core/src/main/java/se/inera/odp/exception/ODPException.java
@@ -0,0 +1,12 @@
+package se.inera.odp.exception;
+
+public class ODPException extends RuntimeException {
+
+	private static final long serialVersionUID = -6084940960373498988L;
+
+	public ODPException(String arg0) {
+		super(arg0);
+	}
+
+	
+}
