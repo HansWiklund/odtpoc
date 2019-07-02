3c95374525e7c7c45c2db4dda8527f07025b71a8
hanswiklund <hans.wiklund@kentor.se>
2019-07-02 08:19:07 +0200

    Added test stub

---
+++ application/src/test/java/se/inera/odp/service/ODPServiceTest.java
@@ -0,0 +1,16 @@
+package se.inera.odp.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class ODPServiceTest {
+
+	
+	@Test
+	public void someTest() {
+		// TODO
+	}
+}
