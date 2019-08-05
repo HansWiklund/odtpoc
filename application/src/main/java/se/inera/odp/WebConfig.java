3e54e6c4c8736301b64f986d84349c871932c506
hanswiklund <hans.wiklund@kentor.se>
2019-08-05 15:45:33 +0200

    Configuration for swagger

---
+++ application/src/main/java/se/inera/odp/WebConfig.java
@@ -0,0 +1,30 @@
+package se.inera.odp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+/**
+ * Required for swagger because of spring.resources.add-mappings=false in application.properties
+ * @author hanwik
+ *
+ */
+public class WebConfig  implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
---
+++ application/src/main/java/se/inera/odp/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package se.inera.odp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${security.enable-csrf}")
+    private boolean csrfEnabled;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if (!csrfEnabled) {
+            http.csrf().disable();
+        }
+    }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**", "/swagger-ui.html", "/webjars/**");
+    }
+}
