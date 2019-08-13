14c0b089378aa68bfb6cc4b14b2f529265f88ee1
hanswiklund <hans.wiklund@kentor.se>
2019-08-13 09:35:59 +0200

    PFOD-227

--- proxy/src/main/java/se/inera/odp/proxy/ODPProxyApplication.java
+++ proxy/src/main/java/se/inera/odp/proxy/ODPProxyApplication.java
@@ -3,7 +3,7 @@ package se.inera.odp.proxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages= {"se.inera.odp", "se.inera.odp.*", "se.inera.odp.core.*"})
+@SpringBootApplication(scanBasePackages= {"se.inera.odp.proxy"})
 public class ODPProxyApplication {
 
     public static void main(String[] args) {
